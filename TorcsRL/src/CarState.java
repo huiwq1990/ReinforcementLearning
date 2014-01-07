e8c371119767324ec93a112581f4f1de47522b04
huiwq1990 <huiwq1990@163.com>
2014-01-07 23:59:18 +0800

    ss

---
+++ TorcsRL/src/CarState.java
@@ -0,0 +1,19 @@
+import scr.SensorModel;
+
+public class CarState {
+	
+	  /**
+	   * Updates the state with the given Environment.
+	   */
+	  public void update(SensorModel sensors) {
+
+	  
+	  }
+
+	public long getStateNumber() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	  
+
+}
