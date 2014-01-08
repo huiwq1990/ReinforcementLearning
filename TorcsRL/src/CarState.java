691a86ae196922d5fc81059c8e3c339591d58b30
huiwq1990 <huiwq1990@163.com>
2014-01-08 17:47:12 +0800

    rl

--- TorcsRL/src/CarState.java
+++ TorcsRL/src/CarState.java
@@ -2,13 +2,34 @@ import scr.SensorModel;
 
 public class CarState {
 	
-	  /**
+	private double changeDistanceFromStartLine = 0;
+	private double lastDistanceFromStartLine=0 ;
+	
+	private double changeAngleToTrackAxis = 0;
+	private double lastAngleToTrackAxis = 0 ;
+	
+	private double changeSpeed = 0;
+	private double lastSpeed = 0 ;
+	 
+	/**
 	   * Updates the state with the given Environment.
 	   */
-	  public void update(SensorModel sensors) {
-
 	  
-	  }
+	public void update(SensorModel sensors) {
+		changeDistanceFromStartLine = sensors.getDistanceFromStartLine() - lastDistanceFromStartLine;
+		lastDistanceFromStartLine = sensors.getDistanceFromStartLine();
+		
+		changeAngleToTrackAxis = sensors.getAngleToTrackAxis() - lastAngleToTrackAxis;
+		lastAngleToTrackAxis = sensors.getAngleToTrackAxis();
+		
+		changeSpeed = sensors.getSpeed() - lastSpeed;
+		lastSpeed = sensors.getSpeed();
+	
+	}
+	
+	public float calculateReward() {
+		
+	}
 
 	public long getStateNumber() {
 		// TODO Auto-generated method stub
